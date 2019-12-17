33684205e2edf8b6a129ebe5eb769acaf78fc68b
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2019-12-17 15:53:43 +0100

    Updates

---
+++ precache-manifest.b9fa6d5eb7f35f204832c132d9b3b4ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d7712bc3a584276bcb9cec539d101e",
+    "url": "/pogodareact/index.html"
+  },
+  {
+    "revision": "527023d259de9ee04acc",
+    "url": "/pogodareact/static/css/main.deb55340.chunk.css"
+  },
+  {
+    "revision": "c9163d8c2dd8114d1633",
+    "url": "/pogodareact/static/js/2.8435ff76.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pogodareact/static/js/2.8435ff76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "527023d259de9ee04acc",
+    "url": "/pogodareact/static/js/main.aca05d5c.chunk.js"
+  },
+  {
+    "revision": "477caeea212778a8f065",
+    "url": "/pogodareact/static/js/runtime-main.69175e68.js"
+  },
+  {
+    "revision": "c0774ba93abf153b39bdf5d227489ee7",
+    "url": "/pogodareact/static/media/background-wallpaper.c0774ba9.jpg"
+  }
+]);
